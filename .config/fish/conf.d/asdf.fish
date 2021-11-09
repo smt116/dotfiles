c91d7c319b350b266092febf49cb7d0127e8482b
Maciej Malecki <maciej@smefju.pl>
2021-11-09 16:28:53 +0100

    Add asdf helper functions for running commands for each plugin version.

--- .config/fish/conf.d/asdf.fish
+++ .config/fish/conf.d/asdf.fish
@@ -1,1 +1,13 @@
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+function asdf-each --wraps asdf --argument-names plugin
+  for plugin_version in (asdf list $plugin)
+    asdf-command $plugin (string trim $plugin_version) $argv[2..-1]
+  end
+end
+
+function asdf-command --wraps asdf --argument-names plugin --argument-names plugin_version
+  asdf shell $plugin $plugin_version
+  eval $argv[3..-1]
+  asdf shell $plugin --unset
+end
